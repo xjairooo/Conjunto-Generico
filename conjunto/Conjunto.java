35c2233c523ce35e0cb8bcf3e066ef6d7beee59d
Jairo Campero <camperojairo0@gmail.com>
2025-04-07 20:19:33 -0300

    completado

--- conjunto/Conjunto.java
+++ conjunto/Conjunto.java
@@ -4,14 +4,14 @@ package conjunto;
 public class Conjunto<T> {
 	private T[] elementos;
 	private int cantidadElementos;
+
 	/**
 	 * Constructor de la clase Conjunto. Inicializa un conjunto vacio con una
 	 * capacidad maxima de 10 elementos.
 	 */
-	// Constructor de la clase Conjunto. Recibe la capacidad máxima del conjunto.
 	public Conjunto(int capacidad) {
-	    elementos = (T[]) new Object[capacidad]; // Legal, pero lanza una advertencia del compilador
-	    cantidadElementos = 0; // Inicialmente no hay elementos en el conjunto
+		elementos = (T[]) new Object[capacidad]; // Legal, pero lanza una advertencia del compilador
+		cantidadElementos = 0; // Inicialmente no hay elementos en el conjunto
 	}
 
 	/**
@@ -37,23 +37,23 @@ public class Conjunto<T> {
 	}
 
 	/**
-	 * Retorna la cantidad de elementos de un conjunto
-	 * 
+	 * @return la cantidad de elementos de un conjunto
 	 */
 	public int tamanioConjunto() {
 		return cantidadElementos;
 	}
 
 	/**
-	 * 	Crea un nuevo conjunto con la union del conjunto actual con otro conjunto.
-	 *  En esa union los elementos nos se repiten
-	 * 	
-	 * 	@param otroConjunto El conjunto con el que se desea realizar la union
-	 *  @return Un nuevo conjunto que contiene la union de los elementos del conjunto
+	 * Crea un nuevo conjunto con la union del conjunto actual con otro conjunto.
+	 * En esa union los elementos nos se repiten
+	 * 
+	 * @param otroConjunto El conjunto con el que se desea realizar la union
+	 * @return Un nuevo conjunto que contiene la union de los elementos del conjunto
 	 * 
 	 */
 	public Conjunto<T> union(Conjunto<T> otroConjunto) {
-		// Creamos un un conjunto para la union con capacidad suficiente para ambos conjuntos
+		// Creamos un un conjunto para la union con capacidad suficiente para ambos
+		// conjuntos
 		Conjunto<T> unionConjunto = new Conjunto<T>(this.cantidadElementos + otroConjunto.cantidadElementos);
 
 		// Primero agregamos elementos del conjunto actual
@@ -62,19 +62,19 @@ public class Conjunto<T> {
 		}
 
 		// Luego agregamos elementos del otro conjunto, evitando duplicados
-		for	(int i = 0; i < otroConjunto.cantidadElementos; i++) {
+		for (int i = 0; i < otroConjunto.cantidadElementos; i++) {
 			T elemento = otroConjunto.elementos[i]; // Obtenemos el elemento del otro conjunto
 			// Verificamos si el elemento ya existe en la union antes de agregarlo
 			if (!unionConjunto.buscarElemento(elemento)) {
 				unionConjunto.insertarElemento(elemento); // Agregamos el elemento a la union si no existe en ella
 			}
 		}
-
 		return unionConjunto;
 	}
 
 	/**
-	 * Crea un nuevo conjunto con la interseccion de los elementos del conjunto actual con otro conjunto.
+	 * Crea un nuevo conjunto con la interseccion de los elementos del conjunto
+	 * actual con otro conjunto.
 	 * 
 	 * @param otroConjunto El conjunto con el que se desea realizar la interseccion
 	 * @return Un nuevo conjunto que contiene la interseccion de los elementos del
@@ -88,29 +88,38 @@ public class Conjunto<T> {
 
 			// Verificamos si el elemento existe en el otro conjunto
 			// Si existe, lo agregamos a la interseccion
-			if (otroConjunto.buscarElemento(elemento)) { 
-				interseccionConjunto.insertarElemento(elemento); // Agregamos el elemento a la interseccion si existe en el otro conjunto
+			if (otroConjunto.buscarElemento(elemento)) {
+				interseccionConjunto.insertarElemento(elemento); // Agregamos el elemento a la interseccion si existe en
+																	// el otro conjunto
 			}
 		}
 		return interseccionConjunto;
 	}
 
 	/**
-	 * Retorna si dos conjuntos tienen los mismos elementos
+	 * Verifica si el conjunto actual es un subconjunto de otro conjunto.
 	 * 
+	 * @param otroConjunto El conjunto con el que se desea comparar
+	 * @return true si el conjunto actual es un subconjunto del otro conjunto, false
+	 *         en caso contrario.
 	 */
 	public boolean iguales(Conjunto<T> otroConjunto) {
-		if (this.cantidadElementos != otroConjunto.cantidadElementos) { // verifica si ambos conjuntos [] tienen diferentes dimensiones
-			return false; //retorna falso
+		if (this.cantidadElementos != otroConjunto.cantidadElementos) { // verifica si ambos conjuntos [] tienen
+																		// diferentes dimensiones
+			return false; // retorna falso
 		}
-		for (int i = 0; i < this.cantidadElementos; i++) { //si ambos tienen la misma dimension osea cantidad de elementos, vamos comprobando si ambos conjuntos tienen los mismo elementos
-			if (otroConjunto.buscarElemento(this.elementos[i])) { //llamo al metodo "buscarElementos" usando el equals para saber si tengo o no el elemento en ambos conjuntos
+		for (int i = 0; i < this.cantidadElementos; i++) { // si ambos tienen la misma dimension osea cantidad de
+															// elementos, vamos comprobando si ambos conjuntos tienen
+															// los mismo elementos
+			if (otroConjunto.buscarElemento(this.elementos[i])) { // llamo al metodo "buscarElementos" usando el equals
+																	// para saber si tengo o no el elemento en ambos
+																	// conjuntos
 				return false;
 			}
 		}
-		return true; //si pudo pasar por ambas condiciones (verificacion de misma cantidad de elementos) y (mismo elementos) entonces retorna true
+		return true; // si pudo pasar por ambas condiciones (verificacion de misma cantidad de
+						// elementos) y (mismo elementos) entonces retorna true
 	}
-
 
 	/**
 	 * Verifica si un elemento esta dentro del conjunto
@@ -129,19 +138,23 @@ public class Conjunto<T> {
 
 	@Override
 	public String toString() {
-		StringBuilder cadena = new StringBuilder(); //creo un objeto cadena para juntar los elementos
-		cadena.append("{"); //imprimo la llave "{" para abrir y comenzar a armar la cadena de elementos del Conjunto
+		StringBuilder cadena = new StringBuilder(); // creo un objeto cadena para juntar los elementos
+		cadena.append("{"); // imprimo la llave "{" para abrir y comenzar a armar la cadena de elementos del
+							// Conjunto
 		for (int i = 0; i < cantidadElementos; i++) {
-			cadena.append(elementos[i]); //pongo el elemento en la cadena e imprimo
-			if (i < cantidadElementos - 1) { /*uso una condicion if para cuando llego al anteultimo elemento de mi array deje de imprimir una coma
-											  cuando llegue al ultimo elemento dejo de imprimir una coma y salgo del for y llegando al objeto cadena para imprimir una llave que cierra "}"*/ 
-					cadena.append(", "); //son los separadores 
+			cadena.append(elementos[i]); // pongo el elemento en la cadena e imprimo
+			if (i < cantidadElementos - 1) { /*
+												 * uso una condicion if para cuando llego al anteultimo elemento de mi
+												 * array deje de imprimir una coma
+												 * cuando llegue al ultimo elemento dejo de imprimir una coma y salgo
+												 * del for y llegando al objeto cadena para imprimir una llave que
+												 * cierra "}"
+												 */
+				cadena.append(", "); // son los separadores
 			}
 		}
-		cadena.append("}"); //cierro con llave la cadena
-		return cadena.toString(); //retorno la cadena ya completada con todo los elementos ya imprimidos
-		}
+		cadena.append("}"); // cierro con llave la cadena
+		return cadena.toString(); // retorno la cadena ya completada con todo los elementos ya imprimidos
+	}
 
 }
-
-
--- conjunto/TestConjunto.java
+++ conjunto/TestConjunto.java
@@ -3,34 +3,26 @@ package conjunto;
 public class TestConjunto {
 
 	public static void main(String[] args) {
-	
 
+		// Creamos dos conjuntos de enteros con capacidad 3
 		Conjunto<Integer> c1 = new Conjunto<Integer>(3);
 		Conjunto<Integer> c2 = new Conjunto<Integer>(3);
 
-		
-		//ingreso de los elementos al 1°conjunto
+		// ingreso de los elementos al 1°conjunto
 		c1.insertarElemento(10);
 		c1.insertarElemento(20);
 		c1.insertarElemento(30);
-		
-		//ingreso de los elementos al 2°conjunto
+
+		// ingreso de los elementos al 2°conjunto
 		c2.insertarElemento(20);
 		c2.insertarElemento(30);
 		c2.insertarElemento(120);
-		
-		
-		//imprimo ambos conjuntos y verificar que esten los elementos ingresados que fueron dados por el usuario 
-		System.out.println ("1° Conjunto: "+c1.toString()+"\t2°Segundo: "+c2.toString());
-		
-		System.out.println("\nUNION: "+c1.union(c2));
-		System.out.println("INTESECCION: "+c1.interseccion(c2));
-		System.out.println("¿AMBOS CONJUNTOS SON IGUALES?: "+c1.iguales(c2));
 
-		
-				
+		// imprimo ambos conjuntos y verificar que esten los elementos ingresados que
+		// fueron dados por el usuario
+		System.out.println("1° Conjunto: " + c1.toString() + "\t2°Segundo: " + c2.toString());
+		System.out.println("\nUNION: " + c1.union(c2));
+		System.out.println("INTESECCION: " + c1.interseccion(c2));
+		System.out.println("¿AMBOS CONJUNTOS SON IGUALES?: " + c1.iguales(c2));
 	}
-	
-	
-
 }
